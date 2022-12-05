a4de125028de6a3dd68e174fce9a84f152633304
Michaël Zasso <targos@protonmail.com>
2022-12-05 17:11:55 +0100

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
+/* eslint-disable camelcase */
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA as vitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePWA({
+      registerType: 'prompt',
+      manifest: {
+        scope: '/',
+        name: 'Galvanicium',
+        short_name: 'Galvanicium',
+        description: 'Galvanicium description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
