e8a6df5c9082798aee47a9f2a382549248ac0a2e
Michaël Zasso <targos@protonmail.com>
2022-12-06 10:12:12 +0100

    update

--- src/pwa/PwaReloadPrompt.tsx
+++ src/pwa/PwaReloadPrompt.tsx
@@ -4,46 +4,31 @@ import { useRegisterSW } from 'virtual:pwa-register/react';
 
 export default function PwaReloadPrompt() {
   const {
-    offlineReady: [offlineReady, setOfflineReady],
     needRefresh: [needRefresh, setNeedRefresh],
     updateServiceWorker,
-  } = useRegisterSW({
-    onRegistered(r) {
-      // eslint-disable-next-line prefer-template
-      console.log('SW Registered: ' + r);
-    },
-    onRegisterError(error) {
-      console.log('SW registration error', error);
-    },
-  });
+  } = useRegisterSW();
 
   const close = () => {
-    setOfflineReady(false);
     setNeedRefresh(false);
   };
 
   return (
     <div className="ReloadPrompt-container">
-      {(offlineReady || needRefresh) && (
+      {needRefresh && (
         <div className="ReloadPrompt-toast">
           <div className="ReloadPrompt-message">
-            {offlineReady ? (
-              <span>App ready to work offline</span>
-            ) : (
-              <span>
-                New content available, click on reload button to update.
-              </span>
-            )}
+            <span>
+              A new version of the application is available. Click on the reload
+              button to update.
+            </span>
           </div>
-          {needRefresh && (
-            <button
-              type="button"
-              className="ReloadPrompt-toast-button"
-              onClick={() => void updateServiceWorker(true)}
-            >
-              Reload
-            </button>
-          )}
+          <button
+            type="button"
+            className="ReloadPrompt-toast-button"
+            onClick={() => void updateServiceWorker(true)}
+          >
+            Reload
+          </button>
           <button
             type="button"
             className="ReloadPrompt-toast-button"
