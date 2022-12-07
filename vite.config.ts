77041006b8ba6b44be8b9eb7a5027321a19de7d3
Michaël Zasso <targos@protonmail.com>
2022-12-07 09:49:42 +0100

    refactor: move logos to subfolder and about dialog

--- vite.config.ts
+++ vite.config.ts
@@ -31,12 +31,12 @@ export default defineConfig({
 
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'logo/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'logo/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
