f814a0611debac6b28008b37ca67e6e9e65b7d1f
Michaël Zasso <targos@protonmail.com>
2022-12-08 13:52:08 +0100

    feat: update app icon
    Closes: https://github.com/zakodium-oss/galvanicium-app/issues/5

--- vite.config.ts
+++ vite.config.ts
@@ -40,6 +40,12 @@ export default defineConfig({
             sizes: '512x512',
             type: 'image/png',
           },
+          {
+            src: 'logo/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
         ],
       },
     }),
